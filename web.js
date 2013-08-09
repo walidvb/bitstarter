e3d961c351942e8982b4dec7c84d3d3071ca9296
Walid <walidvb@gmail.com>
2013-08-09 23:59:45 +0200

    pulled text from index.html

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var file = fs.createReadStream('./index.html');
+var text;
+file.on('data', function(data_){
+	console.log("data:", data_.toString());
+	text = data_.toString('utf-8');
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
