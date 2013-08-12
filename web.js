67f9614f029164d9044ccfbd791851debc6182db
Walid <walidvb@gmail.com>
2013-08-12 14:53:28 +0200

    added grader.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
 var fs = require('fs');
-var file = fs.createReadStream('./index.html');
+
 var text;
 file.on('data', function(data_){
 	console.log("data:", data_.toString());
